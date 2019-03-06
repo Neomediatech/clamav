fd9191aa706914a1c37ba89530a47bdc4b6bcd61
Dario B <feddar@users.noreply.github.com>
2019-03-06 14:48:01 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@ DIR="/var/log/clamav"
 LOGS="$DIR/clamd.log $DIR/freshclam.log"
 mkdir -p $DIR ; chown clamav:clamav $DIR ; touch $LOGS ; chown clamav:clamav $LOGS ; chmod 777 $DIR ; chmod 666 $LOGS
 
+if [ -d /var/lib/clamav ]; then
+  chown clamav:clamav /var/lib/clamav
+fi
+
 exec freshclam -d &
 
 exec tail -f $LOGS &
